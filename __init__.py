5745d8bdbc3f9aa8a852a72ccee7b344ee1a9936
Marinho Kraemer <malmeida@newfields.com>
2019-05-03 10:05:32 -0300

    run

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from flask import Flask 
+from flask import Flask
 
-app = flask(__name__)
+app = Flask(__name__)
 app.config.from_object('settings')
 
 from home import views
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask_script import Manager, Server
-from flask.ext.blog import app
+from flask_blog import app
 
 manager = Manager(app)
 
