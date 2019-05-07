e1e55db626d7939c03532b61696914bcb4acb3f3
Marinho Kraemer <malmeida@newfields.com>
2019-05-07 09:28:58 -0300

    author

--- settings.py
+++ settings.py
@@ -1,2 +1,11 @@
+import os
+
 SECRET_KEY = 'you-will-never-guess'
 DEBUG = True
+DB_USERNAME = 'malmeida'
+DB_PASSWORD = ''
+BLOG_DATABASE_NAME = 'flask_blog'
+DB_HOST = os.getenv('IP', '0.0.0.0')
+DB_URI = "postgresql+psycopg2://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
